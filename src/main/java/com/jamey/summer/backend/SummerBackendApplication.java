f29504af408799282e345b2db0869dbfcc167e70
cpqi <cpqi@163.com>
2020-02-25 01:32:21 +0800

    Porject Initial

---
+++ src/main/java/com/jamey/summer/backend/SummerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jamey.summer.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamey/summer/backend/SummerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamey.summer.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
